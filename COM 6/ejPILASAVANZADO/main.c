588514a9718e44228151c88e6b2d10f7824c938e
melinaceleg <melinaceleg@gmail.com>
2022-04-06 13:18:49 -0300

    pilas avanzado

--- COM 6/ejPILASAVANZADO/main.c
+++ COM 6/ejPILASAVANZADO/main.c
@@ -6,48 +6,48 @@
 ///2. Contar los elementos de una pila (usar variables enteras)
 ///3. Hacer el promedio de los elementos de una pila
 
-//int main()
-//{
-//    Pila DADA;
-//    Pila AUX;
-//    char continuar='n';
-//    float promedio=0;
-//    int suma=0; ///acumulador
-//    int i=0; ///contador
-//    inicpila(&DADA);
-//    inicpila(&AUX);
-//
-//    while(continuar == 's' || continuar == 'S')
-//    {
-//        leer(&DADA);
-//        printf("Desea ingresar otro valor? s/n \n");
-//        fflush(stdin);
-//        scanf("%c",&continuar);
-//
-//    }
-//
-/////conservamos la pila (devolvemos los valores de aux hacia ella)
-//    while (!pilavacia(&DADA))
-//    {
-//        suma = suma + tope(&DADA); /// suma += tope(&DADA);
-//        apilar(&AUX,desapilar(&DADA));
-//        i++;
-//    }
-//
-//    if (i != 0)
-//        promedio = (float)suma/i;
-//
-//    while (!pilavacia(&AUX))
-//    {
-//        apilar(&DADA,desapilar(&AUX));
-//    }
-//    printf("La suma es %i\n", suma);
-//    printf("La cantidad de elementos es %i\n", i);
-//    printf("El promedio es %.2f", promedio);
-//
-//
-//    return 0;
-//}
+int main()
+{
+    Pila DADA;
+    Pila AUX;
+    char continuar='n';
+    float promedio=0;
+    int suma=0; ///acumulador
+    int i=0; ///contador
+    inicpila(&DADA);
+    inicpila(&AUX);
+
+    while(continuar == 's' || continuar == 'S')
+    {
+        leer(&DADA);
+        printf("Desea ingresar otro valor? s/n \n");
+        fflush(stdin);
+        scanf("%c",&continuar);
+
+    }
+
+///conservamos la pila (devolvemos los valores de aux hacia ella)
+    while (!pilavacia(&DADA))
+    {
+        suma = suma + tope(&DADA); /// suma += tope(&DADA);
+        apilar(&AUX,desapilar(&DADA));
+        i++;
+    }
+
+    if (i != 0)
+        promedio = (float)suma/i;
+
+    while (!pilavacia(&AUX))
+    {
+        apilar(&DADA,desapilar(&AUX));
+    }
+    printf("La suma es %i\n", suma);
+    printf("La cantidad de elementos es %i\n", i);
+    printf("El promedio es %.2f", promedio);
+
+
+    return 0;
+}
 
 
 /**
@@ -55,51 +55,51 @@ Encontrar el menor elemento de una pila y guardarlo en otra.
 (sin variables enteras, solo pilas)
 **/
 
-//int main()
-//{
-//    Pila DADA;
-//    Pila AUX;
-//    Pila BASURA;
-//    char continuar;
-//    Pila MENOR;
-//    inicpila(&MENOR);
-//    inicpila(&DADA);
-//    inicpila(&AUX);
-//    inicpila(&BASURA);
-//    do
-//    {
-//        leer(&DADA);
-//        printf("Desea ingresar otro valor?s\n /n");
-//        fflush(stdin);
-//        scanf("%c",&continuar);
-//
-//    }
-//    while(continuar == 's' || continuar == 'S');
-//
-//
-//    apilar(&MENOR,tope(&DADA));
-//    apilar(&AUX,desapilar(&DADA));
-//    while (!pilavacia(&DADA))
-//    {
-//        if (tope(&MENOR) > tope(&DADA))
-//        {
-//            apilar(&BASURA,desapilar(&MENOR));
-//            apilar(&MENOR,tope(&DADA));
-//            apilar(&AUX,desapilar(&DADA));
-//        }
-//        else
-//        {
-//            apilar(&AUX,desapilar(&DADA));
-//        }
-//    }
-//
-//    while(!pilavacia(&AUX))
-//    {
-//        apilar(&DADA,desapilar(&AUX));
-//    }
-//
-//    return 0;
-//}
+int main()
+{
+    Pila DADA;
+    Pila AUX;
+    Pila BASURA;
+    char continuar;
+    Pila MENOR;
+    inicpila(&MENOR);
+    inicpila(&DADA);
+    inicpila(&AUX);
+    inicpila(&BASURA);
+    do
+    {
+        leer(&DADA);
+        printf("Desea ingresar otro valor?s\n /n");
+        fflush(stdin);
+        scanf("%c",&continuar);
+
+    }
+    while(continuar == 's' || continuar == 'S');
+
+
+    apilar(&MENOR,tope(&DADA));
+    apilar(&AUX,desapilar(&DADA));
+    while (!pilavacia(&DADA))
+    {
+        if (tope(&MENOR) > tope(&DADA))
+        {
+            apilar(&BASURA,desapilar(&MENOR));
+            apilar(&MENOR,tope(&DADA));
+            apilar(&AUX,desapilar(&DADA));
+        }
+        else
+        {
+            apilar(&AUX,desapilar(&DADA));
+        }
+    }
+
+    while(!pilavacia(&AUX))
+    {
+        apilar(&DADA,desapilar(&AUX));
+    }
+
+    return 0;
+}
 
 /**
 Usando lo resuelto en el ejercicio 4,
@@ -108,60 +108,60 @@ quede ordenada de mayor (tope) a menor (base).
 Esto se llama método de ordenación por selección.
 **/
 
-//
-//int main()
-//{
-//    Pila DADA;
-//    Pila AUX;
-//    char continuar;
-//    Pila MENOR;
-//    inicpila(&MENOR);
-//    inicpila(&DADA);
-//    inicpila(&AUX);
-//    do
+
+int main()
+{
+    Pila DADA;
+    Pila AUX;
+    char continuar;
+    Pila MENOR;
+    inicpila(&MENOR);
+    inicpila(&DADA);
+    inicpila(&AUX);
+    do
+    {
+        leer(&DADA);
+        printf("Desea ingresar otro valor?s/n \n");
+        fflush(stdin);
+        scanf("%c",&continuar);
+
+    }
+    while(continuar == 's' || continuar == 'S');
+
+
+    while (!pilavacia(&DADA))
+    {
+        apilar(&MENOR,desapilar(&DADA));
+
+        while (!pilavacia(&DADA)) ///busco el menor y lo paso a la otra pila (lo elimino de dada)
+        {
+            if (tope(&MENOR) > tope(&DADA))
+            {
+                apilar(&AUX,desapilar(&MENOR));
+                apilar(&MENOR,desapilar(&DADA));
+            }
+            else
+            {
+                apilar(&AUX,desapilar(&DADA));
+            }
+        }
+        while (!pilavacia(&AUX)) ///devuelvo los restantes (los que estan en aux) a dada
+        {
+            apilar(&DADA,desapilar(&AUX));
+        }
+    }
+
+    mostrar(&MENOR);
+
+//    while (!pilavacia(&MENOR))
 //    {
-//        leer(&DADA);
-//        printf("Desea ingresar otro valor?s/n \n");
-//        fflush(stdin);
-//        scanf("%c",&continuar);
-//
+//        apilar(&DADA,desapilar(&MENOR));
 //    }
-//    while(continuar == 's' || continuar == 'S');
-//
-//
-//    while (!pilavacia(&DADA))
-//    {
-//        apilar(&MENOR,desapilar(&DADA));
-//
-//        while (!pilavacia(&DADA)) ///busco el menor y lo paso a la otra pila (lo elimino de dada)
-//        {
-//            if (tope(&MENOR) > tope(&DADA))
-//            {
-//                apilar(&AUX,desapilar(&MENOR));
-//                apilar(&MENOR,desapilar(&DADA));
-//            }
-//            else
-//            {
-//                apilar(&AUX,desapilar(&DADA));
-//            }
-//        }
-//        while (!pilavacia(&AUX)) ///devuelvo los restantes (los que estan en aux) a dada
-//        {
-//            apilar(&DADA,desapilar(&AUX));
-//        }
-//    }
-//
-//    mostrar(&MENOR);
-//
-////    while (!pilavacia(&MENOR))
-////    {
-////        apilar(&DADA,desapilar(&MENOR));
-////    }
-////    mostrar(&DADA);
-//
-//
-//    return 0;
-//}
+//    mostrar(&DADA);
+
+
+    return 0;
+}
 /**
 
 Insertar un elemento en una pila ordenada de menor (tope) a mayor (base)
