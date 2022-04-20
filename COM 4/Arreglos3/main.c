7d5c4bceafae4280ba159537097516fa2a485a8c
melinaceleg <melinaceleg@gmail.com>
2022-04-20 12:31:16 -0300

    Arreglos - Ordenacion por seleccion:

--- COM 4/Arreglos3/main.c
+++ COM 4/Arreglos3/main.c
@@ -6,6 +6,8 @@
 void cargaArreglo(int[], int*); ///recibo el arreglo por referencia y los validos por referencia
 void muestraArreglo(int[],int); ///recibo el arreglo por referencia (no hay otra forma) y los validos
 int sumaArreglo(int[],int); ///recibo el arreglo por referencia y los validos
+void ordenacionSeleccion(int[], int validos);
+int buscaMenor(int[],int,int); ///funcion que busca el menor desde una celda hasta la ultima
 
 int sumaArreglo(int arreglo[],int validos)
 {
@@ -45,6 +47,39 @@ void cargaArreglo(int arreglo[], int* validos)
     *validos = i;
 }
 
+int buscaMenor(int arreglo[],int i,int validos)
+{
+    int indiceMenor = i;
+    for (int j = i+1; j < validos; j++)
+    {
+        if (arreglo[j] < arreglo[indiceMenor])
+        {
+            indiceMenor = j;
+        }
+    }
+    return indiceMenor;
+}
+
+void ordenacionSeleccion(int arreglo[], int validos)
+{
+    int indiceMenor;
+    int aux;
+    for (int i = 0 ; i < validos-1; i++)
+    {
+        ///busqueda del indice que contiene el menor elemento, partiendo desde la celda i
+        indiceMenor= buscaMenor(arreglo,i,validos);
+
+        ///proceso de intercambio
+        aux = arreglo[i];
+        arreglo[i] = arreglo[indiceMenor];
+        arreglo[indiceMenor] = aux;
+
+    }
+
+}
+
+
+
 int main()
 {
     int arreglo[CANT_MAX];
@@ -54,11 +89,42 @@ int main()
 
     cargaArreglo(arreglo,&validos);
     muestraArreglo(arreglo,validos);
-    suma=sumaArreglo(arreglo,validos);
 
-    printf("El promedio es %.2f", (float)suma/validos);
+///ordenacion por seleccion
+/// - iterador que recorria celda por celda
+///     -> desde esa celda buscaba el menor -> intercambiaba el valor de esa celda con la celda del menor
 
-    printf("\nCantidad de datos cargados %i", validos);
+int indiceMenor;
+int aux;
+    ///recorro todo el arreglo hasta el anteultimo
+    for (int i = 0; i < validos-1; i++)
+    {
+        ///proceso de buscar el menor desde i+1 hasta hasta la ultima celda, considerando al principio como indiceMenor i
+        indiceMenor = i;
+        for (int j = i+1 ; j < validos; j++)
+        {
+            if (arreglo[j] < arreglo[indiceMenor])
+            {
+              indiceMenor = j;
+            }
+        }
+        ///proceso de intercambio entre la celda i y la celda indiceMenor
+        aux = arreglo[i];
+        arreglo[i] = arreglo[indiceMenor];
+        arreglo[indiceMenor] = aux;
+
+    }
+
+
+
+
+
+
+//    suma=sumaArreglo(arreglo,validos);
+//
+//    printf("El promedio es %.2f", (float)suma/validos);
+//
+//    printf("\nCantidad de datos cargados %i", validos);
 
 
     return 0;
