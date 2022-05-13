ec04bcc0441e412de177ab2b7a8225b87d17ed0c
melinaceleg <melinaceleg@gmail.com>
2022-05-13 14:17:18 -0300

    Matrices

--- COM 1/Matrices/main.c
+++ COM 1/Matrices/main.c
@@ -108,123 +108,123 @@ void muestraMatriz(int matriz[][COL], int validosF, int validosC)
 
 int main()
 {
-//   int matriz[FIL][COL] = {{4,8,10},
-//                       {5,7,6},
-//                       {8,9,10},
-//                       {7,1,15}};
-//
-//   ///carga por teclado (por el usuario) de una matriz completa
-//
-//    for (int i = 0; i < FIL ; i++)
-//    {
-//        for (int j = 0; j < COL ; j++)
-//        {
-//            printf("matriz[%i][%i]=", i,j);
-//            scanf("%i", &matriz[i][j]);
-//        }
-//        printf("\n");
-//    }
-//
-//    ///muestra en formato matricial
-//    for (int i = 0 ; i < FIL ; i++)
-//    {
-//        for (int j = 0 ; j < COL ; j++)
-//        {
-//            printf("%i ", matriz[i][j]);
-//        }
-//
-//        printf("\n");
-//    }
-//
+   int matriz[FIL][COL] = {{4,8,10},
+                       {5,7,6},
+                       {8,9,10},
+                       {7,1,15}};
+
+   ///carga por teclado (por el usuario) de una matriz completa
+
+    for (int i = 0; i < FIL ; i++)
+    {
+        for (int j = 0; j < COL ; j++)
+        {
+            printf("matriz[%i][%i]=", i,j);
+            scanf("%i", &matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+    ///muestra en formato matricial
+    for (int i = 0 ; i < FIL ; i++)
+    {
+        for (int j = 0 ; j < COL ; j++)
+        {
+            printf("%i ", matriz[i][j]);
+        }
+
+        printf("\n");
+    }
 
 
-    /// puedo no cargar completamente mi matriz, eligiendo no cargar todas las filas
-    /// voy a tener un validos de filas
-    ///el usuario cargara por filas y decidira cuando cortar
-    ///las columnas por cada fila se cargan completamente (COL)
 
-//    int matriz[FIL][COL];
-//    int validosF;
-//
-//    char continuar;
-//    int i=0;
-//
-//    do
-//    {
-//        printf("FILA %i\n", i);
-//        for (int j = 0 ; j < COL ; j++)
-//        {
-//            printf("matriz[%i][%i]=",i,j);
-//            scanf("%i", &matriz[i][j]);
-//        }
-//
-//        i++;
-//        printf("Desea cargar otra fila?\n");
-//        fflush(stdin);
-//        scanf("%c",&continuar);
-//
-//    }
-//    while(i<FIL && continuar == 's');
-//
-//    validosF = i; ///cantidad de filas CARGADAS
-//
-//    ///muestra de la matriz
-//
-//    for (int i = 0 ; i < validosF ; i++)
-//    {
-//        for (int j = 0 ; j < COL ; j++)
-//        {
-//            printf("%i ", matriz[i][j]);
-//        }
-//        printf("\n");
-//    }
+/// puedo no cargar completamente mi matriz, eligiendo no cargar todas las filas
+/// voy a tener un validos de filas
+///el usuario cargara por filas y decidira cuando cortar
+///las columnas por cada fila se cargan completamente (COL)
 
-    ///Que pasa si ahora quiero implementar cargar solo ciertas columnas y ciertas filas?
-    /// validosF y validosC
-    /// de antemano debo saber cuantas columnas el usuario quiere cargar.
+    int matriz[FIL][COL];
+    int validosF;
 
-//    int matriz[FIL][COL];
-//    int validosF;
-//    int validosC;
-//    char continuar;
-//    int i = 0;
-//
-//    printf("cuantas columnas desea que se carguen?\n");
-//    scanf("%i", &validosC);
-//    while ( validosC > COL)
-//    {
-//        printf("Debe ingresar una cantidad de columnas menor o igual a %i: ", COL);
-//        scanf("%i", &validosC);
-//    }
-//
-//
-//    do
-//    {
-//        printf("FILA %i\n", i);
-//        for (int j = 0 ; j < validosC ; j++)
-//        {
-//            printf("matriz[%i][%i]=",i,j);
-//            scanf("%i", &matriz[i][j]);
-//        }
-//        i++;
-//        printf("Desea cargar otra fila?\n");
-//        fflush(stdin);
-//        scanf("%c", &continuar);
-//    }
-//    while (i < FIL && continuar == 's');
-//
-//    validosF = i;
-//
-//    ///muestra matriz
-//    for (int i = 0 ; i < validosF ; i++)
-//    {
-//        for (int j = 0 ; j < validosC ; j++)
-//        {
-//            printf("%i ", matriz[i][j]);
-//        }
-//
-//        printf("\n");
-//    }
+    char continuar;
+    int i=0;
+
+    do
+    {
+        printf("FILA %i\n", i);
+        for (int j = 0 ; j < COL ; j++)
+        {
+            printf("matriz[%i][%i]=",i,j);
+            scanf("%i", &matriz[i][j]);
+        }
+
+        i++;
+        printf("Desea cargar otra fila?\n");
+        fflush(stdin);
+        scanf("%c",&continuar);
+
+    }
+    while(i<FIL && continuar == 's');
+
+    validosF = i; ///cantidad de filas CARGADAS
+
+    ///muestra de la matriz
+
+    for (int i = 0 ; i < validosF ; i++)
+    {
+        for (int j = 0 ; j < COL ; j++)
+        {
+            printf("%i ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+///Que pasa si ahora quiero implementar cargar solo ciertas columnas y ciertas filas?
+/// validosF y validosC
+/// de antemano debo saber cuantas columnas el usuario quiere cargar.
+
+    int matriz[FIL][COL];
+    int validosF;
+    int validosC;
+    char continuar;
+    int i = 0;
+
+    printf("cuantas columnas desea que se carguen?\n");
+    scanf("%i", &validosC);
+    while ( validosC > COL)
+    {
+        printf("Debe ingresar una cantidad de columnas menor o igual a %i: ", COL);
+        scanf("%i", &validosC);
+    }
+
+
+    do
+    {
+        printf("FILA %i\n", i);
+        for (int j = 0 ; j < validosC ; j++)
+        {
+            printf("matriz[%i][%i]=",i,j);
+            scanf("%i", &matriz[i][j]);
+        }
+        i++;
+        printf("Desea cargar otra fila?\n");
+        fflush(stdin);
+        scanf("%c", &continuar);
+    }
+    while (i < FIL && continuar == 's');
+
+    validosF = i;
+
+    ///muestra matriz
+    for (int i = 0 ; i < validosF ; i++)
+    {
+        for (int j = 0 ; j < validosC ; j++)
+        {
+            printf("%i ", matriz[i][j]);
+        }
+
+        printf("\n");
+    }
 
     ///yo podria luego volver a cargar otra fila entonces validosF se veria modificado
     ///pero validosC una vez que yo elegi el valor NO SE PUEDE ALTERAR MAS
